939b2be7a53f784723d142ca29338ec6fc73745e
RamaVnaik <ramakrishnavarthe@gmail.com>
2023-04-17 16:33:38 +0530

    commit all the components scripts and config

--- mongo.sh
+++ mongo.sh
@@ -2,4 +2,5 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-# 127.0.0.1 to 0.0.0.0 in
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf /etc/mongod/mongod.conf
+
