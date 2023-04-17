60a8493d40fd4162546760af5d6e411996cf91e1
RamaVnaik <ramakrishnavarthe@gmail.com>
2023-04-17 17:14:43 +0530

    commit catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,16 @@
+echo -e "\e[36m<<<<<<<configuring nohdejs repos >>>>>>>>\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[36m<<<<<<< installing nodejs >>>>>>>>>>\e[0m"
 yum install nodejs -y
+echo -e "\e[36m<<<<<<<<< application user >>>>>>>>>\e[0m"
+
 useradd roboshop
+echo -e "-e[36m<<<<<<<< create application directory\e[0m"
 mkdir /app
+echo -e "\e[36m<<<<<<<<< download app content >>>>>>>>>\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+
 unzip /tmp/catalogue.zip
 cd /app
 npm install
